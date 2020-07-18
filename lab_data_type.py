cbe6b6263ce01fbd9f21f5a542e46971c69595f4
Boris <50569248+borko81@users.noreply.github.com>
2020-07-18 15:48:31 +0300

    Add files via upload

---
+++ lab_data_type.py
@@ -0,0 +1,45 @@
+# 01. Concat Names
+
+# name_one = input()
+# name_two = input()
+# concat_str = input()
+
+# print(f'{name_one}{concat_str}{name_two}')
+
+# 02. Centuries to Minutes
+
+# number = int(input())
+
+# year = number * 100
+# days = year * 365.2422
+# hours = int(days) * 24
+# minutes = int(hours) * 60
+
+# print(f'{number} centuries = {year} years = {int(days)} days = {int(hours)} hours = {minutes} minutes')
+
+# 03. Special Numbers
+
+# number = int(input())
+
+# for _ in range(1, number + 1):
+#     sum_of_digit = 0
+#     diggit = _
+#     while diggit > 0:
+#         sum_of_digit += diggit % 10
+#         diggit = diggit // 10
+       
+#     if (sum_of_digit == 5) or (sum_of_digit == 7) or (sum_of_digit == 11):
+#         print(f'{_} -> True')
+#     else:
+#         print(f'{_} -> False')
+
+# 04. Convert Meters to Kilometers
+
+# meters = int(input())
+# print(f'{meters/1000:.2f}')
+
+# 05. Pounds to Dollars
+
+# pound = int(input())
+
+# print(f'{pound*1.31:.3f}')
