b9643df3bba677035a3ceccdd1a973e5021583ab
Borko <korea60@abv.bg>
2020-07-20 19:37:09 +0300

    Lists Basics - Lab

---
+++ Lists_Basics_Lab.py
@@ -0,0 +1,90 @@
+# 02. Courses
+# n = int(input())
+# result = []
+# for _ in range(n):
+#     line = input()
+#     result.append(line)
+#
+# print(result)
+
+# 01. Strange Zoo
+# tail = input()
+# body = input()
+# head = input()
+#
+# result = []
+# result.append(tail)
+# result.append(body)
+# result.append(head)
+#
+# print(list(reversed(result)))
+
+# 03. Lists Statistics
+# n = int(input())
+# positive = []
+# negative = []
+#
+# for i in range(n):
+#     l = int(input())
+#     if l >= 0:
+#         positive.append(l)
+#     else:
+#         negative.append(l)
+#
+# print(positive)
+# print(negative)
+# print(f'Count of positives: {len(positive)}. Sum of negatives: {sum(negative)}')
+
+# 04. Search
+# n = int(input())
+# word = input()
+# result = []
+#
+# for i in range(n):
+#     l = input()
+#     result.append(l)
+#
+# print(result)
+# print([i for i in result if word in i])
+
+# 05. Numbers Filter
+# n = int(input())
+# result = []
+#
+# for i in range(n):
+#     l = int(input())
+#     result.append(l)
+#
+# command = input()
+#
+# if command == 'even':
+#     print([i for i in result if i % 2 == 0])
+# elif command == 'odd':
+#     print([i for i in result if i % 2 == 1])
+# elif command == 'negative':
+#     print([i for i in result if i < 0])
+# else:
+#     print([i for i in result if i >= 0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
